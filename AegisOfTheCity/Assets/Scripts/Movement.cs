bd86c247f71b02e8bec10993081a6a5206487b8e
Daluur <fknudsen@gmail.com>
2014-03-27 14:16:33 +0100

    minor tweaks
    added a box, changed movement

--- AegisOfTheCity/Assets/Scripts/Movement.cs
+++ AegisOfTheCity/Assets/Scripts/Movement.cs
@@ -2,29 +2,33 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-	float x=0.0f;
-	float y=0.0f;
-	float z=0.0f;
+	float xd=0.0f;
+	float yd=0.0f;
+	private Vector3 moveDirection;
 	// Use this for initialization
 	void Start () {
-	
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	if (Input.GetKey("w")) {
-						y += 1.0f;
-				}
-	if (Input.GetKey("d")) {
-			x += 1.0f;
+		yd = 0;
+		xd = 0;
+		if (Input.GetKey("w")) {
+			yd = 1.0f;
+		}
+		if (Input.GetKey("d")) {
+			xd = 1.0f;
 		}
 		if (Input.GetKey("s")) {
-			y -= 1.0f;
+			yd = -1.0f;
 		}
 		if (Input.GetKey("a")) {
-			x -= 1.0f;
+			xd = -1.0f;
 		}
-		transform.position = new Vector2(x,y);
+
+		moveDirection = new Vector3(xd,yd,0).normalized;
+
+		transform.position += moveDirection;
 	}
 }
