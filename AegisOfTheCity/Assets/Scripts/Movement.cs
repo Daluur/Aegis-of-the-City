2c5a28e3b7548d997229e90ea6d1e748b13c9138
mfynbo12 <mfynbo12@student.aau.dk>
2014-04-02 18:23:33 +0200

    Improved player movement
    improved player movement script..
    
    note: use arrow keys now

--- AegisOfTheCity/Assets/Scripts/Movement.cs
+++ AegisOfTheCity/Assets/Scripts/Movement.cs
@@ -2,34 +2,24 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-	float xd=0.0f;
-	float yd=0.0f;
-	private Vector3 moveDirection;
+	
+	public Vector2 speed = new Vector2(12, 12);
+	private Vector2 movement;
+	
+	void Update()
+	{
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// 3 - Movement per direction
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+	}
 
-	// Use this for initialization
-	void Start () {
-
+	void FixedUpdate(){
+		rigidbody2D.velocity = movement;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		yd = 0;
-		xd = 0;
-		if (Input.GetKey("w")) {
-			yd = 0.01f; //1.0f
-		}
-		if (Input.GetKey("d")) {
-			xd = 0.01f;//1.0f
-		}
-		if (Input.GetKey("s")) {
-			yd = -0.01f;//1.0f
-		}
-		if (Input.GetKey("a")) {
-			xd = -0.01f;//1.0f
-		}
-
-		moveDirection = new Vector2(xd,yd).normalized;
-
-		transform.position += moveDirection;
-	}
+	
 }
