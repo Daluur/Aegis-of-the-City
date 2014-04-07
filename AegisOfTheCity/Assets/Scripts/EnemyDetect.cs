a9e923be4eea0f2cdaf6ff1c37454f6881be5c0d
mfynbo12 <mfynbo12@student.aau.dk>
2014-04-07 17:17:38 +0200

    Enemy scripting
    New enemy prefab, with a simple follow script.
    When a player enters its field of view it now follows him around untill
    killed.

---
+++ AegisOfTheCity/Assets/Scripts/EnemyDetect.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDetect : MonoBehaviour {
+	
+	private bool detected;
+	private Vector2 dir;
+	private float dirX;
+	private float dirY;
+	private Vector2 player;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (detected == true) {
+			print ("yes");
+			player = GameObject.FindWithTag ("Player").transform.position;
+			dirX = (player.x - transform.position.x);
+			dirY = (player.y - transform.position.x);
+			dir = new Vector2 (dirX,dirY);
+			
+			rigidbody2D.velocity = dir;
+		} else {
+			print ("no");
+			rigidbody2D.Sleep();
+		}
+		
+	}
+	
+	void FixedUpdate(){
+		//rigidbody2D.velocity = movement;
+	}
+	
+	void OnTriggerEnter2D (Collider2D coll) {
+		if (coll.gameObject.tag == "Player") {
+			detected = true;
+		}
+	}
+}
