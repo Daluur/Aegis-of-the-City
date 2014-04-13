6773771f8139d206ea2be2a1ec781596db67d1f4
blacou12 <blacou12@student.aau.dk>
2014-04-13 16:57:18 +0200

    Menu screen, win and lose screen aswell with working buttons to either start or quit the game

---
+++ AegisOfTheCity/Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown(){
+
+		if (collider2D.gameObject.name == "startbutton")
+						Application.LoadLevel (1);
+		if (collider2D.gameObject.name == "Quitbutton") {
+			Debug.Log("I quit");
+			Application.Quit ();
+
+				}
+	}
+	
+}
---
+++ AegisOfTheCity/Assets/Scripts/Score.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+	public int score = 23243;
+	// Use this for initialization
+	void Start () {
+		guiText.text = "Score: " + score.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
